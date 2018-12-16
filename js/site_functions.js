e9916f9b57a796c2cca4099389e60946a39c1ca3
jhancock1975 <jhancock1975@gmail.com>
2018-12-16 12:11:28 -0500

    Update site_functions.js

--- js/site_functions.js
+++ js/site_functions.js
@@ -14,7 +14,14 @@ get_posts = async (post_element_id) => {
   posts_div = document.getElementById('blog_posts');
   posts_div.innerText = postsJson;
 }
-github_api_call = async() => {
+/**
+ * Convenience method for making API call.
+ * 
+ * @author: jhancock1975
+ * @param api_method: github api endpoint method, example: repos/contents
+ * @param api_params: github method parameters, example: posts
+ */
+github_api_call = async(api_method, api_params) => {
   url_str = site_settings[base_url]+'/'+site_settings[user_id] + '/' + site_settings[repo_name] 
     + '/' + api_method + '/' + api_params;
   resposne = await fetch(url_str);
