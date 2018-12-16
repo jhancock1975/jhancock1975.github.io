51a96e0a0da9c9fe68e9c85da6cf176c0e90043b
jhancock1975 <jhancock1975@gmail.com>
2018-12-16 12:19:07 -0500

    Update site_functions.js

--- js/site_functions.js
+++ js/site_functions.js
@@ -18,10 +18,11 @@ get_posts = async (post_element_id) => {
  * Convenience method for making API call.
  * 
  * @author: jhancock1975
- * @param api_method: github api endpoint method, example: repos/contents
+ * @param api_category: github API category name, example: repos
+ * @param api_method: github API endpoint method, example: repo/contents
  * @param api_params: github method parameters, example: posts
  */
-github_api_call = async(api_method, api_params) => {
+github_api_call = async(api_category, api_method, api_params) => {
   url_str = site_settings[base_url]+'/'+site_settings[user_id] + '/' + site_settings[repo_name] 
     + '/' + api_method + '/' + api_params;
   response = await fetch(url_str);
