ced8a34e42c0da3a0a5959a4a602456b4ad29279
jhancock1975 <jhancock1975@gmail.com>
2018-12-16 08:42:13 -0500

    Update site_functions.js

--- js/site_functions.js
+++ js/site_functions.js
@@ -1,8 +1,14 @@
+site_settings = async() => {
+   url_str = 'https://api.github.com/repos/' + user_id + '/jhancock1975.github.io/js/site_settings.json/';
+   response = await(url_str)
+   site_settings = await response.json();
+   console.debug('site settings ', site_settings');
+}
 list_files = async (user_id) => {
    url_str = 'https://api.github.com/repos/' + user_id + '/jhancock1975.github.io/contents/';
    response = await fetch(url_str);
-   myJson = await response.json(); //extract JSON from the http response
-  console.log("myJson ", myJson)
+   file_list = await response.json(); //extract JSON from the http response
+  console.log("file_list ", myJson)
 }
 get_posts = async (user_id, post_element_id) => {
    url_str = 'https://api.github.com/repos/' + user_id + '/jhancock1975.github.io/contents/';
