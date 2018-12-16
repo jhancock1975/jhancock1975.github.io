a04ea2e5fc97e639e8dbf2f8f42ec6bbfadf1d4b
jhancock1975 <jhancock1975@gmail.com>
2018-12-16 09:05:03 -0500

    Update site_functions.js

--- js/site_functions.js
+++ js/site_functions.js
@@ -1,13 +1,20 @@
-const site_settings = await site_settings();
-list_files = async (user_id) => {
-   url_str = 'https://api.github.com/repos/' + user_id + '/jhancock1975.github.io/contents/';
-   response = await fetch(url_str);
-   file_list = await response.json(); //extract JSON from the http response
+site_settings = {'user_id' : 'jhancock_1975', 
+                 'repo_name' : 'jhancock1975.github.io', 
+                 'posts_location' : 'posts'};
+
+list_files = async () => {
+  url_str = 'https://api.github.com/repos/' + site_settings['user_id'] + '/' 
+      + site_settings['repo_name'] + '/contents/';
+  response = await fetch(url_str);
+  file_list = await response.json(); //extract JSON from the http response
   console.log("file_list ", myJson)
 }
-get_posts = async (user_id, post_element_id) => {
-   url_str = 'https://api.github.com/repos/' + user_id + '/jhancock1975.github.io/contents/';
-   response = await fetch(url_str);
-   myJson = await response.json(); //extract JSON from the http response
-  console.log("myJson ", myJson)
+
+get_posts = async (post_element_id) => {
+  url_str = 'https://api.github.com/repos/' + site_settings['user_id'] + '/' 
+      + site_settings['repo_name'] +'/contents/';
+  response = await fetch(url_str);
+  postsJson = await response.json(); //extract JSON from the http response
+  posts_div = document.getElementById('posts');
+  posts.innerText = postsJson;
 }
