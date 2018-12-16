138365f4223ee5b5f5b923bfb342535adc14b228
jhancock1975 <jhancock1975@gmail.com>
2018-12-16 14:34:36 -0500

    Update site_functions.js

--- js/site_functions.js
+++ js/site_functions.js
@@ -1,26 +1,11 @@
-list_files = async () => {
-  url_str = 'https://api.github.com/repos/' + site_settings['user_id'] + '/' 
-      + site_settings['repo_name'] + '/contents/';
-  response = await fetch(url_str);
-  file_list = await response.json();
-  console.log("file_list ", myJson)
-}
 
-get_posts = async (post_element_id) => {
-  url_str = 'https://api.github.com/repos/' + site_settings['user_id'] + '/' 
-      + site_settings['repo_name'] +'/contents/';
-  response = await fetch(url_str);
-  postsJson = await response.json();
-  posts_div = document.getElementById('blog_posts');
-  posts_div.innerText = postsJson;
-}
 /**
  * Convenience method for making API call.
  * 
  * @author: jhancock1975
  * @param api_category: github API category name, example: repos
- * @param api_method: github API endpoint method, example: repo/contents
- * @param api_params: github method parameters, example: posts
+ * @param api_method: github API endpoint method, example: contents
+ * @param api_params: github method parameters, example: posts (the name of some directory)
  */
 github_api_call = async(api_category, api_method, api_params) => {
   url_str = site_settings[base_url]+'/'+ api_category + '/' + site_settings[user_id] 
@@ -28,4 +13,24 @@ github_api_call = async(api_category, api_method, api_params) => {
   response = await fetch(url_str);
   response_json = await response.json();
   return response_json;
+}
+/**
+ * This function gets blog posts from a github repository
+ * it expects there to be a directory called posts in the
+ * top level of the repository, and attaches the posts
+ * to child divs of postDiv
+ */
+get_posts = async(postDiv) => {
+   github_api_call('repos', 'contents', 'posts')
+     .then((blog_posts) => {
+       blog_posts.map((blog_post) => {
+         github_api_call('repos', 'contents', 'posts'+'/'+blog_post['name'])
+           .then((blog_post) => {
+             console.log(blog_post);
+             var newPostDiv = document.createElement('div');
+             newPostDiv.innerHTML = atob(blog_post.content);
+             postDiv.appendChild(newPostDiv);
+         })
+       })
+   })
 }
