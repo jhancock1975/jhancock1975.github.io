152a5bc03d9a3f30e0f5cfef562b68bbf2e3f739
jhancock1975 <jhancock1975@gmail.com>
2018-12-16 08:35:26 -0500

    Update site_functions.js

--- js/site_functions.js
+++ js/site_functions.js
@@ -1,6 +1,12 @@
 list_files = async (user_id) => {
    url_str = 'https://api.github.com/repos/' + user_id + '/jhancock1975.github.io/contents/';
-   response = await fetch('https://api.github.com/repos/jhancock1975/jhancock1975.github.io/contents/');
+   response = await fetch(url_str);
+   myJson = await response.json(); //extract JSON from the http response
+  console.log("myJson ", myJson)
+}
+get_posts = async (user_id, post_element_id) => {
+   url_str = 'https://api.github.com/repos/' + user_id + '/jhancock1975.github.io/contents/';
+   response = await fetch(url_str);
    myJson = await response.json(); //extract JSON from the http response
   console.log("myJson ", myJson)
 }
