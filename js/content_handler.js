cc7c76cd9f777dc0aedca0cbe94dd7800c6ec670
jhancock1975 <jhancock1975@gmail.com>
2018-12-16 22:40:14 -0500

    Update content_handler.js

--- js/content_handler.js
+++ js/content_handler.js
@@ -3,6 +3,13 @@
  */
  
 //load blog posts from github into blog_posts div:
+get_posts(document.getElementById('blog_posts'));
+
+/**
+ * event handler for when OAUTH text box token is set
+ *
+ * @param tokenText: DOM text input element, mean to hold an OAUTH token
+ */
 fetchPosts = (tokenText) => {
  get_posts(document.getElementById('blog_posts'), tokenText.value);
 }
