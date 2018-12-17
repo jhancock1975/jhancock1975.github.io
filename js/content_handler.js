f48d54bf0456b0d9907aaf46e3d6c0e777bbaab7
jhancock1975 <jhancock1975@gmail.com>
2018-12-16 22:19:53 -0500

    Update content_handler.js

--- js/content_handler.js
+++ js/content_handler.js
@@ -4,6 +4,6 @@
  
 //load blog posts from github into blog_posts div:
 get_posts(document.getElementById('blog_posts'), document.getElementById('tokenText').value);
-fetch_posts = () =>{
- console.debug(this.value);
+fetch_posts = (tokenTextName) =>{
+ console.debug(document.getElementById(tokenTextName).value);
 }
