3663bff6eec16bfbdb6fc9ea1db8533ca4e05613
John Hancock <jhancock1975@gmail.com>
2018-12-17 22:22:04 -0500

    stop loading posts in constructor, change semantics of fetchPosts parameter

--- js/ContentHandler.js
+++ js/ContentHandler.js
@@ -2,15 +2,17 @@
 * uses functionality defined in site_functions.js
 */
 function ContentHandler(){
- gitHubBroker = new GitHubBroker();
- //load blog posts from github into blog_posts div:
- gitHubBroker.get_posts(document.getElementById('blog_posts'));
+  var gitHubBroker = new GitHubBroker();
+  var oauthToken; 
+  setOauthToken = (tokenValue) => {
+    oauthToken = tokenValue; 
+ }
 }
 /**
  * fetches post using OAUTH token for authentication
  *
  * @param tokenText: DOM text input element, mean to hold an OAUTH token
  */
-ContentHandler.prototype.fetchPosts = (tokenText) => {
-  gitHubBroker.get_posts(document.getElementById('blog_posts'), tokenText.value);
+ContentHandler.prototype.fetchPosts = (blogPostsDiv) => {
+  gitHubBroker.get_posts(blogPostsDiv, oauthToken);
  }
