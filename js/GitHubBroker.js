62a9f7df0947e60bf78e36a789e790e5cb6b9035
John Hancock <jhancock1975@gmail.com>
2018-12-20 23:11:18 -0500

    finish api call refactor, fix typo on blog.html page

--- js/GitHubBroker.js
+++ js/GitHubBroker.js
@@ -1,109 +1,4 @@
 function GitHubBroker(){
-  /**
-   * Convenience method for making API call. So far this covers get requests
-   * in the repos family
-   * 
-   * @author: jhancock1975
-   * @param api_category: github API category name, example: repos
-   * @param api_method: github API endpoint method, example: contents
-   * @param api_params: github method parameters, example: posts (the name of
-   * some directory)
-   */
-  github_get_request = async(api_category, api_method, api_params, tokenText) => {
-    url_str = site_settings[base_url]+'/'+ api_category + '/'
-      + site_settings[user_id] 
-      + '/' + site_settings[repo_name] + '/' + api_method + '/' + api_params;
-    console.debug('url_str = ', url_str);
-    var headers_obj = {'Accept': 'application/vnd.github.v3+json'};
-    if ((tokenText !== undefined) && (tokenText !== "")){
-      console.debug('setting oath token value ', tokenText);
-      headers_obj['Authorization'] = 'token ' + tokenText;
-    } else {
-      console.debug('The token_val is blank or undefined.');
-      console.debug('I am not using authorization for API calls.');
-    }
-    var response = await fetch(url_str, 
-                           {method : 'GET', 
-                            headers: headers_obj
-                           });
-    var response_json = await response.json();
-    return response_json;
-  }
-
-  /**
-   * Convenience method for making API call.  The need for this method came
-   * about when we started working on post method that gets a blob
-   * 
-   * @author: jhancock1975
-   * @param api_category: github API category name, example: repos
-   * @param api_method: github API endpoint method, example: contents
-   * @param api_params: github method parameters, example: posts (the name of
-   * some directory)
-   */
-  github_post_request = async(api_category, api_method, api_params, tokenText,
-    post_data) => {
-    console.log('post_data = ', post_data);
-    url_str = site_settings[base_url]+'/'+ api_category 
-      + '/' + site_settings[user_id]
-      + '/' + site_settings[repo_name] + '/' + api_method ;
-    if ((api_params !== undefined) && (api_params !== "")){
-      url_str = url_str + '/' + api_params;
-    }
-    console.debug('url_str = ', url_str);
-    var headers_obj = {'Accept': 'application/vnd.github.v3+json'};
-    if ((tokenText !== undefined) && (tokenText !== "")){
-      console.debug('setting oauth token value ', tokenText);
-      headers_obj['Authorization'] = 'token ' + tokenText;
-    } else {
-      console.debug('The token_val is blank or undefined.');
-      console.debug('I am not using authorization for API calls.');
-    }
-    var response = await fetch(url_str, 
-                           {method : 'POST', 
-                            headers: headers_obj,
-                            body: post_data
-                           });
-    var response_json = await response.json();
-    return response_json;
-  }
-
-  /**
-   * Convenience method for making API call.  The need for this method came
-   * about when we started working on updateHead, which does a PATCH
-   * 
-   * @author: jhancock1975
-   * @param api_category: github API category name, example: repos
-   * @param api_method: github API endpoint method, example: contents
-   * @param api_params: github method parameters, example: posts (the name of
-   * some directory)
-   */
-  github_patch_request = async(api_category, api_method, api_params, tokenText,
-    patch_data) => {
-    console.log('patch_data = ', patch_data);
-    url_str = site_settings[base_url]+'/'+ api_category 
-      + '/' + site_settings[user_id]
-      + '/' + site_settings[repo_name] + '/' + api_method ;
-    if ((api_params !== undefined) && (api_params !== "")){
-      url_str = url_str + '/' + api_params;
-    }
-    console.debug('url_str = ', url_str);
-    var headers_obj = {'Accept': 'application/vnd.github.v3+json'};
-    if ((tokenText !== undefined) && (tokenText !== "")){
-      console.debug('setting oauth token value ', tokenText);
-      headers_obj['Authorization'] = 'token ' + tokenText;
-    } else {
-      console.debug('The token_val is blank or undefined.');
-      console.debug('I am not using authorization for API calls.');
-    }
-    var response = await fetch(url_str, 
-                           {method : 'PATCH', 
-                            headers: headers_obj,
-                            body: patch_data
-                           });
-    var response_json = await response.json();
-    return response_json;
-  }
-
   const body = 'body';
   const contents = 'contents';
   /**
@@ -125,7 +20,7 @@ function GitHubBroker(){
 
     let headers_obj = {'Accept': 'application/vnd.github.v3+json'};
 
-    if ((tokenText !== undefined) && (tokenText !== "")){
+    if ((oauthToken !== undefined) && (oauthToken!== "")){
       console.debug(dbg_tag, 'setting oauth token value ', oauthToken);
       headers_obj['Authorization'] = 'token ' + oauthToken;
     } else {
@@ -227,8 +122,12 @@ function GitHubBroker(){
   getCurrentCommitSHA = async(oauthToken, getCurrentTreeSHA) => {
     console.debug('getting currrent commit sha');
     console.debug('oauth token = ', oauthToken);
-    var ref = await github_get_request('repos', 'git/refs',
-      'heads/'+site_settings[branch_name], oauthToken);
+    let ref = await github_api_call(
+      'repos'
+      + '/' + site_settings[user_id]
+      + '/' + site_settings[repo_name] 
+      + '/git/refs/heads/'
+      +site_settings[branch_name], 'GET', oauthToken);
     return ref.object.sha;
     } 
 
@@ -243,8 +142,12 @@ function GitHubBroker(){
     getCurrentTreeSHA = async(oauthToken, sha) =>{
       console.debug("getCurrentTreeSHA::oauthToken = ", oauthToken);
       console.debug("getCurrentTreeSHA::sha= ", sha);
-      var commit = await github_get_request('repos', 'git/commits',
-        sha, oauthToken);
+      let commit = await github_api_call(
+          'repos'
+        + '/' + site_settings[user_id]
+        + '/' + site_settings[repo_name] 
+        + '/' + 'git/commits'
+        + '/' + sha, 'GET', oauthToken);
       console.debug('getCurrentTreeSha:: tree sha = ', commit.tree.sha);
       return commit.tree.sha;
     }
@@ -260,7 +163,12 @@ function GitHubBroker(){
   * @param sha - should be SHA value of current tree
   */
    createBlob = async(post_text, oauthToken) => {
-     var blob = await github_post_request('repos', 'git/blobs', "",
+     let blob = await github_api_call(
+      'repos'
+        + '/' + site_settings[user_id]
+        + '/' + site_settings[repo_name] 
+        +  '/git/blobs',
+       'POST',
        oauthToken, 
        JSON.stringify({content: btoa(post_text), 
          encoding: 'base64'}));
@@ -277,13 +185,19 @@ function GitHubBroker(){
    *   token from Github
    */
   createTree = async(tree_sha, blob_sha, oauthToken) => {
-    var blob_obj = [{sha: blob_sha, 
+    let blob_obj = [{sha: blob_sha, 
       path: 'posts/'+new Date().getTime() + ".html",
       mode: '100644',
       type: 'blob'}];
-   var to_push = {tree: blob_obj, base_tree: tree_sha}; 
-   var new_commit_tree_obj = await github_post_request('repos', 'git/trees',"",
-     oauthToken, JSON.stringify(to_push));
+   let to_push = {tree: blob_obj, base_tree: tree_sha}; 
+   let new_commit_tree_obj = await github_api_call(
+       'repos'
+        + '/' + site_settings[user_id]
+        + '/' + site_settings[repo_name] 
+        + '/git/trees',
+        'POST',
+        oauthToken, 
+        JSON.stringify(to_push));
    console.debug(new_commit_tree_obj);
    return new_commit_tree_obj.sha; 
   }
@@ -301,8 +215,14 @@ function GitHubBroker(){
       tree: new_commit_tree_sha,
       parents : [commit_sha]};
     let post_data = JSON.stringify(commit_data);
-    let commit_obj = await github_post_request('repos', 'git/commits', "",
-      oauthToken, post_data);
+    let commit_obj = await github_api_call(
+      'repos'
+      + '/' + site_settings[user_id]
+      + '/' + site_settings[repo_name] 
+      + '/git/commits', 
+      'POST',
+      oauthToken, 
+      post_data);
     console.debug('createCommit::commit_obj ', commit_obj);
     return commit_obj.sha;
   } 
@@ -317,8 +237,13 @@ function GitHubBroker(){
 
     let patch_data = JSON.stringify({sha: new_commit_sha});
 
-    let result = await github_patch_request('repos', 'git/refs', 
-      'heads/'+site_settings[branch_name],
+    let result = await github_api_call(
+      'repos'
+      + '/' + site_settings[user_id]
+      + '/' + site_settings[repo_name] 
+      + '/git/refs/heads/' 
+      +site_settings[branch_name],
+      'PATCH',
       oauthToken, 
       patch_data);
 
