9e8b2bb30ef4fdbd48b0f95cb2cffaa7efafc7c8
John Hancock <jhancock1975@gmail.com>
2019-01-11 08:36:39 -0500

    add to-do list that saves in local storage

---
+++ js/todo.js
@@ -0,0 +1,45 @@
+/**
+  * original code at: https://codepen.io/ragzor/pen/xGrJrg
+  * accessed January 11th 2019
+  */
+function TodoList() {
+  
+  var list = document.querySelector('#list'),
+      form = document.querySelector('form'),
+      item = document.querySelector('#item');
+  
+  form.addEventListener('submit',function(e){
+    e.preventDefault();
+    list.innerHTML += '<li>' + item.value + '</li>';
+    store();
+    item.value = "";
+  },false)
+  
+  list.addEventListener('click',function(e){
+    var t = e.target;
+    if(t.classList.contains('checked')){
+      t.parentNode.removeChild(t);
+    } else {
+      t.classList.add('checked');
+    }
+    store();
+  },false)
+  
+  function store() {
+    window.localStorage.myitems = list.innerHTML;
+  }
+  
+  function getValues() {
+    var storedValues = window.localStorage.myitems;
+    if(!storedValues) {
+      list.innerHTML = '<li>Make a to do list</li>'+
+                       '<li>Check off first thing on the to do list</li>'+
+                       '<li>Realize you have already accomplished 2 things in the list</li>'+
+                       '<li>Reward yourself with a nap</li>';
+    }
+    else {
+      list.innerHTML = storedValues;
+    }
+  }
+  getValues();
+};
